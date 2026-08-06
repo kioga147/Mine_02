0f6fef2f146c115470d6047773ab1b9c82fe964b
kioga147 <1830346431@qq.com>
2026-08-06 12:20:31 +0800

    恢复本地修改

--- Script/Common/ue_enum_custom.lua
+++ Script/Common/ue_enum_custom.lua
@@ -2,17 +2,3 @@
 
 -- sorted by enum name asc 
 
----@enum EGiftPackOpenType
-EGiftPackOpenType = { 
-    ManuallyOpen = 0,
-    AutoOpen = 1,
-}; 
-
-
----@enum EGiftPackType
-EGiftPackType = { 
-    Normal = 0,
-    Optional = 1,
-}; 
-
-
